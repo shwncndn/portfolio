1ac08d3fe857a00d03317102813cf0c3b8e8f0a6
Shawn Condon <shwn.cndn@gmail.com>
2024-03-21 19:06:25 -0400

    updated ui

--- src/components/ProjectHeader.svelte
+++ src/components/ProjectHeader.svelte
@@ -11,7 +11,7 @@
 <header class='sticky z-[10] top-0 duration-200 dark:font-normal px-6'>
   <div class="sm:flex items-center justify-between hidden mx-auto max-w-2xl text-2xl">
     {#each tabs as tab, index}
-      <a href={tab.link} class="duration-200 hover:text-sky-400 dark:hover:text-rose-200">
+      <a href={tab.link} class="duration-200 hover:text-sky-400 dark:hover:text-sky-200">
         <p>{tab.name}</p>
       </a>
     {/each}
